47436a5d9bed1908cc285f0e1080c5be169c558d
pritam <pritam.damania@fb.com>
2020-02-25 11:34:02 -0800

    Unify gradient accumulation between distributed autograd and local autograd engine.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33214
    
    Distributed autograd had some custom logic in terms of how we
    accumulated gradients. This was mostly done early on to enable basic
    functionality. Although, in the long term we should merge this logic with what
    we have in the local autograd engine. A lot of work has gone into ensuring we
    accumulate grads correctly and efficiently and we should reuse that as a
    starting point.
    
    We can investigate if we need further custom logic for distributed autograd
    later on if we need additional optimizations.
    
    In this PR I've merged the gradient accumulation logic and also the gradient
    hooks. As a result, now gradient hooks are called in distributed autograd as
    well.
    ghstack-source-id: 98946495
    
    Differential Revision: [D19843284](https://our.internmc.facebook.com/intern/diff/D19843284/)

--- torch/csrc/autograd/functions/accumulate_grad.h
+++ torch/csrc/autograd/functions/accumulate_grad.h
@@ -11,7 +11,29 @@ struct TORCH_API AccumulateGrad : public Node {
 
   variable_list apply(variable_list&& grads) override;
 
+  // Given a variable with its current grad as variable_grad, accumulates
+  // new_grad into variable_grad if in place accumulation is possible.
+  // Otherwise, uses 'update_grad' to update the grad for the variable.
+
+  // variable: the variable whose grad we're accumulating.
+  // variable_grad: the current grad for the variable.
+  // new_grad: new grad we want to acummulate for the variable.
+  // num_expected_refs: the number of refs we expect to hold internally
+  //                    such that it is safe to avoid cloning the grad
+  //                    if use_count() of the grad is less than or equal
+  //                    to this value (in addition to post_hooks).
+  // update_grad: Function that is used to update grad for the variable.
+  //              The argument to the function is a Tensor which
+  //              is used to set a new value for the grad.
+  static void accumulateGradAndCallHooks(
+      const Variable& variable,
+      at::Tensor variable_grad,
+      const at::Tensor& new_grad,
+      size_t num_expected_refs,
+      const std::function<void(at::Tensor&&)>& update_grad);
+
   Variable variable;
 };
 
-}} // namespace torch::autograd
+} // namespace autograd
+} // namespace torch
